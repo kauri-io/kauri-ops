bc8f1f604dfa7a5b803a67286fc95e16b24a8ab6
joshorig <cassidy.joshua@gmail.com>
2018-03-22 23:58:48 +0000

    FLOW-132 update get next version

--- versioning/get-next-dev-ver.sh
+++ versioning/get-next-dev-ver.sh
@@ -3,12 +3,9 @@
 #get highest tags across all branches, not just the current branch
 VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
 
-# split into array
-VERSION_BITS=(${VERSION//./ })
-
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
-VNUM3=${VERSION_BITS[2]}
+VNUM1="$(cut -d'.' -f1 <<<"$VERSION")"
+VNUM2="$(cut -d'.' -f2 <<<"$VERSION")"
+VNUM3="$(cut -d'.' -f3 <<<"$VERSION")"
 VNUM3=$((VNUM3+1))
 
 #create new version
