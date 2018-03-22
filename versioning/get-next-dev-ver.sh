d51cdcdc8a37612b06c184278f2e56ab14b3752c
joshorig <cassidy.joshua@gmail.com>
2018-03-22 20:51:13 +0000

    FLOW-132 add script to bump next dev version

--- versioning/get-next-dev-ver.sh
+++ versioning/get-next-dev-ver.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-echo "Getting next dev version based on hihest tag"
-
 #get highest tags across all branches, not just the current branch
 VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
 
 # split into array
 VERSION_BITS=(${VERSION//./ })
-
-echo "Latest version tag: $VERSION"
 
 VNUM1=${VERSION_BITS[0]}
 VNUM2=${VERSION_BITS[1]}
@@ -16,5 +12,5 @@ VNUM3=${VERSION_BITS[2]}
 VNUM3=$((VNUM3+1))
 
 #create new version
-export NEW_DEV_VERSION="$VNUM1.$VNUM2.$VNUM3"
-echo "Exported new version as NEW_DEV_VERSION=$NEW_DEV_VERSION"
+NEW_DEV_VERSION="$VNUM1.$VNUM2.$VNUM3"
+echo $NEW_DEV_VERSION
