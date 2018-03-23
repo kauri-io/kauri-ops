a71d02020c444025a1bdbdfe93d4d1ab3e0b9064
joshorig <cassidy.joshua@gmail.com>
2018-03-23 22:12:48 +0000

    FLOW-150: fix: update GKE provisioning script

--- gke/provision.sh
+++ gke/provision.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #
 #
-# Provision resources in Azure
+# Provision resources in Google Cloud
 
 # Source library
 source ../lib.sh
@@ -27,41 +27,22 @@ load_config
 check_required_env_vars
 [[ $? -ne 0 ]] && return 1
 
-print_banner 'Start provisioning container registry...'
-#az group create -n ${c_group} -l ${WEST_EUROPE}
-#create_container_registry ${c_group}
-
 print_banner 'Start provisioning of kubenetes resources...'
 
-#create_kubernetes_gke ${ACS_NAME} ${K8_MASTER_COUNT} ${K8_MASTER_VM} ${K8_AGENT_COUNT} ${K8_AGENT_VM} ${K8S_VERSION}
+create_kubernetes_gke ${ACS_NAME} ${K8_MASTER_COUNT} ${K8_MASTER_VM} ${K8_AGENT_COUNT} ${K8_AGENT_VM} ${K8S_VERSION}
 
-#gcloud iam service-accounts create registry-account --display-name "Registry service account"
-#gcloud projects add-iam-policy-binding kauri \
-#   --member serviceAccount:registry-account@kauri-197812.iam.gserviceaccount.com --role project/viewer
+gcloud container clusters get-credentials $ACS_NAME --zone $GOOGLE_ZONE
 
-#gcloud iam service-accounts keys create \
-#       ~/key.json \
-#       --iam-account registry-account@kauri-197812.iam.gserviceaccount.com
-
-kubectl create secret docker-registry regsecret \
-                                         --docker-server=https://gcr.io \
-                                         --docker-username=_json_key \
-                                         --docker-email 'example@example.com' \
-                                         --docker-password=”$(cat ~/key.json)”
-
-#print_banner 'Creating secrets and config map in Kubernetes...'
-#create_secrets_in_kubernetes_aks ${k8_group} ${ACS_NAME} ${c_group}
-
+if [ -z "$(kubectl get ns ${TARGET_ENV} --ignore-not-found)" ]; then
+  kubectl create ns ${TARGET_ENV} --save-config
+fi
+kubectl config set-context $(kubectl config current-context) --namespace=${TARGET_ENV}
 
 print_banner 'Initialising Helm & Tiller...'
-#helm init --upgrade
+helm init --upgrade
 
 
 #Set up environment variables for local dev environment
-#source ../env_setup.sh "$@"
-
-#print_banner 'Creating service principle for travisCI...'
-#pwd=$(date +%s | sha256sum | base64 | head -c 32)
-#az ad sp create-for-rbac --name travis-${TARGET_ENV} --password ${pwd}
+source ../env_setup.sh "$@"
 
 print_banner 'Provision completed'
