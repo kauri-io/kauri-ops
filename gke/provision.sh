548b26f12b48a67ff7e6ceee7aa8a6c631838eb9
joshorig <cassidy.joshua@gmail.com>
2018-03-26 11:47:52 +0100

    FLOW-150: fix: add config for GKE setup

--- gke/provision.sh
+++ gke/provision.sh
@@ -39,6 +39,7 @@ fi
 kubectl config set-context $(kubectl config current-context) --namespace=${TARGET_ENV}
 
 kubectl create clusterrolebinding --user system:serviceaccount:kube-system:default kube-system-cluster-admin --clusterrole cluster-admin
+kubectl create clusterrolebinding --user system:serviceaccount:kube-system:kubernetes-dashboard kube-system-cluster-dashboard --clusterrole cluster-admin
 kubectl create clusterrolebinding --user system:serviceaccount:${TARGET_ENV}:default ${TARGET_ENV}-cluster-edit --clusterrole edit
 
 print_banner 'Initialising Helm & Tiller...'
