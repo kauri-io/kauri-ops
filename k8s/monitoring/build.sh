a60eb25c1eece147e36723a445075e12c0ce0541
gjeanmart <gregoire.jeanmart@gmail.com>
2019-10-01 09:21:32 +0100

    pod affinity to equitably balance the monioring pods

--- k8s/monitoring/build.sh
+++ k8s/monitoring/build.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "${TARGET_ENV}" == "" ]; then
+if [[ -z "${TARGET_ENV}" ]]; then
   echo "Environment not set, please run env_setup script in ops folder"
   exit 1
 fi
